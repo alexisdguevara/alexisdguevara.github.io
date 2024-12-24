d368ff32d3705983816502f367bc24c7e51ec3db
chavezpe <chavezpe@usc.edu>
2024-12-23 16:58:14 -0800

    Portfolio

---
+++ assets/python/collect_portfolios.py
@@ -0,0 +1,36 @@
+import os
+import json
+
+
+portfolio_dir = 'assets/portfolio'
+
+projects = []
+
+for folder in os.listdir(portfolio_dir):
+
+    if folder == '.DS_Store':
+        continue
+
+    folder_path = os.path.join(portfolio_dir, folder)
+    
+    images = [f for f in os.listdir(folder_path) if f.endswith('.png') or f.endswith('.jpg')]
+    
+    images.sort()
+    
+    creation_time = os.path.getctime(folder_path)
+    
+    projects.append({
+        'name': folder,
+        'images': images,
+        'creation_time': creation_time
+    })
+        
+projects.sort(key=lambda x: x['creation_time'], reverse=True)
+
+# Delete creation_time key
+for project in projects:
+    del project['creation_time']
+
+with open('assets/portfolio.json', 'w') as f:
+    json.dump(projects, f)
+    
